bb2d78fae053570b3f41509bf0109bd0465564c5
Rémi <remi.becquaert35@gmail.com>
2021-03-24 16:41:26 +0100

    add few changes

--- src/Taquin.java
+++ src/Taquin.java
@@ -3,11 +3,29 @@ import java.awt.event.*;
 import java.util.ArrayList;
 
 public class Taquin extends JDialog {
+    //attributs du .form
     private JPanel contentPane;
     private JList listeDeBoutons;
     private JButton recommencerButton;
-    private ArrayList<JButton> liste;
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+    private JButton button4;
+    private JButton button5;
+    private JButton button6;
+    private JButton button7;
+    private JButton button8;
+    private JButton button9;
+    private JButton button10;
+    private JButton button11;
+    private JButton button12;
+    private JButton button13;
+    private JButton button14;
+    private JButton button15;
+    private JButton button16;
+    private ArrayList<Object> listBtn;
 
+    //attributs privés
 
     public static void main(String[] args) {
         Taquin dialog = new Taquin();
@@ -37,9 +55,39 @@ public class Taquin extends JDialog {
         }, KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
     }
 
+    private void créerJeu(){
+    //creer le tableau et les boutons
+    }
+    private void initListBtn(){
+        listBtn = new ArrayList<>();
+        listeBtn.add(button1);
+        listeBtn.add(button2);
+        listeBtn.add(button3);
+        listeBtn.add(button4);
+
+        listeBtn.add(button5);
+        listeBtn.add(button6);
+        listeBtn.add(button7);
+        listeBtn.add(button8);
+
+        listeBtn.add(button9);
+        listeBtn.add(button10);
+        listeBtn.add(button11);
+        listeBtn.add(button12);
+
+        listeBtn.add(button13);
+        listeBtn.add(button14);
+        listeBtn.add(button15);
+        listeBtn.add(button16);
+
+    }
+
+    private void bougerBouton(){
+    //permet de bouger le bouton si un voisin = ""
+    }
 
     private void winCase(){
-
+    //verifie si le jeu est gagné avec une boucle
     }
 
     private void onRestart() {
@@ -50,4 +98,6 @@ public class Taquin extends JDialog {
         // add your code here if necessary
         dispose();
     }
+
+
 }
